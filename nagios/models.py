7b656967b275fdf2fadd55e0d9cadc941a93e188
Elias Colmenarez <elias.colmenarez@inter.com.ve>
2023-09-19 19:10:59 +0000

    modelos de Host y HostGroups

--- nagios/models.py
+++ nagios/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 
-from django.db.models.signals import post_save, pre_delete
-from django.dispatch import receiver
+# from django.db.models.signals import post_save, pre_delete
+# from django.dispatch import receiver
 
+from decouple import config
 import subprocess
 
+NAGIOS_DIR = config('NAGIOS_DIR')
 
-# Create your models here.
 class Host(models.Model):
     ip = models.GenericIPAddressField(
         verbose_name = 'IP',
@@ -23,11 +24,12 @@ class Host(models.Model):
     )
 
     alias = models.CharField(
-        verbose_name = 'Hostname',
+        verbose_name = 'Descripcion del host',
         max_length   = 100,
         db_column    = 'NagHstDsc',
     )
 
+    # TODO hostgroup debe soportar multiples valores
     parents = models.ForeignKey(
         'nagios.Host', 
         verbose_name = 'Padre', 
@@ -42,11 +44,19 @@ class Host(models.Model):
         db_column    = 'NagHstTem',
     )
 
+    # TODO hostgroup debe soportar multiples valores
+    hostgroup = models.ForeignKey(
+        'nagios.HostGroup',
+        verbose_name = 'Hostgroup', 
+        on_delete    = models.SET_NULL,
+        db_column    = 'NagHstGrpID',
+        null=True, blank=True
+    )
+
     def __str__(self):
         return self.hostname
     
-    def create_nagios_cfg(self, write_parent:bool=True):
-        print('ACTUALIZANDO CFG', self)
+    def create_nagios_cfg(self, write_parent:bool=True, write_hostgroup:bool=True):
         cfg_fields = {
             '# idtag'  : self.id,
             'address'  : self.ip,
@@ -57,35 +67,48 @@ class Host(models.Model):
 
         if self.parents and write_parent:
             cfg_fields['parents'] = self.parents
+
+        if self.hostgroup and write_hostgroup:
+            cfg_fields['hostgroups'] = self.hostgroup
         
         cfg_content = 'define host {\n'
-
         for key, value in cfg_fields.items():
             cfg_content += f'{key}\t{value}\n'
 
         cfg_content += '}'
         
-        with open(f'/usr/local/nagios/etc/objects/hosts/{self.hostname}.cfg', 'w') as f:
+        with open(f'{NAGIOS_DIR + self.hostname}.cfg', 'w') as f:
             f.write(cfg_content)
 
         subprocess.run('systemctl restart nagios', shell=True)
 
 
-@receiver(post_save, sender=Host)
-def update_nagios_hosts(sender, instance:Host, created, **kwargs):
-    instance.create_nagios_cfg()
 
-@receiver(pre_delete, sender=Host)
-def delete_nagios_hosts(sender, instance:Host, **kwargs):
-    subprocess.run(f'rm /usr/local/nagios/etc/objects/hosts/{instance.hostname}.cfg', shell=True)
+class HostGroup(models.Model):
+    nombre = models.CharField(
+        verbose_name = 'Nombre',
+        max_length   = 50,
+        unique       = True,
+        db_column    = 'NagHstGrpNom',
+    )
 
-    print(instance)
+    alias = models.CharField(
+        verbose_name = 'Descripcion del grupo',
+        max_length   = 100,
+        db_column    = 'NagHstGrpDsc',
+    )
 
-    [host.create_nagios_cfg(write_parent=False) for host in Host.objects.filter(parents=instance)]
+    def __str__(self) -> str:
+        return self.nombre
 
+    def crear_nagios_cfg(self):
+        cfg_dict = {'hostgroup_name':self.nombre, 'alias':self.alias}
+        cfg_str = 'define hostgroup {\n'
+        for key, value in cfg_dict.items():
+            cfg_str += f'{key}\t{value}\n'
+        cfg_str += '}'
 
-    subprocess.run('systemctl restart nagios', shell=True)
+        with open(f'{NAGIOS_DIR}{self.nombre}.cfg', 'w') as f:
+            f.write(cfg_str)
 
-
-Host(ip='192.168.0.1', hostname='router', alias='Wifi casa', template='linux-server').save()
-Host(ip='192.168.0.101', hostname='elias-iPhone', alias='iPhone de Elias', template='linux-server', parents=Host.objects.get(ip='192.168.0.1')).save()
+        subprocess.run('systemctl restart nagios', shell=True)
