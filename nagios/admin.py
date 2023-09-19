cf2a7b8230f24fa6d5fe0234ef170d177d501fb3
Elias Colmenarez <elias.colmenarez@inter.com.ve>
2023-09-19 19:08:09 +0000

    registrados Host y HostGroup en el panel de administrador

--- nagios/admin.py
+++ nagios/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Host, HostGroup
 
-# Register your models here.
+@admin.register(Host)
+class HostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'hostname', 'ip', 'alias')
+
+@admin.register(HostGroup)
+class HostGroupAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre', 'alias')
