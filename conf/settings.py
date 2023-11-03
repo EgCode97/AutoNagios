76f2710ca94168cab9a450566dea5fa51401504d
Elias Colmenarez <elias.colmenarez@inter.com.ve>
2023-11-03 21:31:04 +0000

    agregada app core

--- conf/settings.py
+++ conf/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'nagios.apps.NagiosConfig'
+    'nagios.apps.NagiosConfig',
+    'core.apps.CoreConfig'
 ]
 
 MIDDLEWARE = [
